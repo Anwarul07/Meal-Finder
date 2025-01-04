3be6dac9cf421c3bdd969032c57cc4e4d388d064
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-01-05 00:06:20 +0530

    Create MealFind.js

---
+++ MealFind.js
@@ -0,0 +1,10 @@
+let nextBtn = document.getElementById("nextBtn");
+let prevBtn = document.getElementById("prevBtn");
+let allDish = document.querySelectorAll(".dishs");
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+let  dishValue = document.querySelectorAll(".dishVal");
+
+
+
+
